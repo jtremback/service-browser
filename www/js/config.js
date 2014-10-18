d24a117b5ef4fb271711fc5f5f34488e8969702b
Jehan <jehan.tremback@gmail.com>
2014-10-18 14:24:17 -0700

    get rid of crusty 'ol www folder

--- www/js/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  icons: {
-    storage: 'glyphicon-cloud glyphicon',
-    messaging: 'glyphicon glyphicon-envelope',
-    sound: 'glyphicon glyphicon-headphones',
-    book: 'glyphicon glyphicon-book'
-  },
-  columnNum: 4
-}

ca9ec44484af9fbd85400965161b870d143b1632
Jehan <jehan.tremback@gmail.com>
2014-10-18 14:28:13 -0700

    add configurable file references

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ module.exports = {
   http_port: parseInt(process.env.HTTP_PORT, 10) || 8001,
   sync_port: parseInt(process.env.SYNC_PORT, 10) || 8002,
   // mdns_port: parseInt(process.env.MDNS_PORT, 10) || 8003,
-  feed_file: path.resolve(__dirname, './feed.json'),
-  peer_file: path.resolve(__dirname, './peers.json')
+  feed_file: path.resolve(__dirname, process.env.FEED_FILE || './feed.json'),
+  peer_file: path.resolve(__dirname, process.env.PEER_FILE || './peers.json')
 };
